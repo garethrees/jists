e4bd757af7416e41ccdbc9c112d60750738c1c96
Gareth Rees <gareth.h.rees@gmail.com>
2012-07-05 21:26:18 +0100

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source :rubygems
+
+# Rails
+gem 'rails', '3.2.2'
+gem 'mysql2'
+gem 'jquery-rails'
+
+# App
+gem 'grit'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jists::Application.config.secret_token = 'c51210610c817740879e7a876d204334ab92142b55a08998282cf95f0f8e77cc5212579ac75517d725a7dd7997feefe6eea916a5b2c530d0d5a6845dec945421'
